c3045aaf72be1a40797e7b3987f70d1549be6793
Bryant Meskill <bryant.meskill@gmail.com>
2021-12-18 05:03:55 -0600

    Drawing divs done, heavily WIP.

--- scriptEtch.js
+++ scriptEtch.js
@@ -1,1 +1,9 @@
-console.log("Hello!");
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 16; i++) {
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add("grid");
+    container.appendChild(gridDiv);
+  }
+}
