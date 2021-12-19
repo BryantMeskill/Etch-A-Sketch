48edc9b971fccab1ba9447ec7ce9a01df14ef8ea
Bryant Meskill <bryant.meskill@gmail.com>
2021-12-19 16:27:33 -0600

    Moved code into a function.

--- scriptEtch.js
+++ scriptEtch.js
@@ -1,9 +1,16 @@
 const container = document.querySelector(".container");
-
-for (let i = 0; i < 16; i++) {
+//currently calling it manually, will need to set parameter for user input in newer version
+function createGrid() {
   for (let i = 0; i < 16; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.classList.add("grid");
-    container.appendChild(gridDiv);
+    for (let i = 0; i < 16; i++) {
+      const gridDiv = document.createElement("div");
+      gridDiv.classList.add("grid");
+      container.appendChild(gridDiv);
+      gridDiv.addEventListener("mouseover", () => {
+        gridDiv.classList.add("trail");
+      });
+    }
   }
 }
+
+createGrid();
